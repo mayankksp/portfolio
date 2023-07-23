a312c54efdac85d824372cd3c9991f099a51fae3
Mayank Kashyap <contact@mayankkashyap.in>
2023-07-23 22:12:23 +0530

    Fix apos bug

--- components/MainSection.js
+++ components/MainSection.js
@@ -12,7 +12,7 @@ const MainSection = () => {
                 <div className='bg-magical-yellow rounded-md'><Icon className='p-1' icon="icon-park-outline:hamburger-button" color="white" width="36.98" height="35.94" /></div>
             </div>
             <h1 className='font-wonder-magic text-6xl mt-12 px-12'>I am a <br /><span className='text-magical-yellow'>Full Stack Web Dev</span></h1>
-            <p className='px-12 leading-7 mt-4'>Hello, I am Mayank Kashyap. I'm currently in my final year of studies at Chandigarh University, Punjab. I have a passion for developing software applications, a field that I have come to love. There's nothing more fulfilling to me than the realization of my imagination into functional, tangible software. This satisfaction fuels my enthusiasm and dedication towards my work in computer science.</p>
+            <p className='px-12 leading-7 mt-4'>Hello, I am Mayank Kashyap. I&apos;m currently in my final year of studies at Chandigarh University, Punjab. I have a passion for developing software applications, a field that I have come to love. There&apos;s nothing more fulfilling to me than the realization of my imagination into functional, tangible software. This satisfaction fuels my enthusiasm and dedication towards my work in computer science.</p>
             <Image src='/images/Center Image.png' alt='arrow-down' width='241' height='248' className='mt-12 z-10 hovering-animation' />
             <Snowfall color='#FFC70A' snowflakeCount={20}
                 style={{ position: 'absolute', top: 700, left: 120, width: 250, height: 350 }}
